74da80ae7add70f560e487a226e1dca12cd234c7
Dylan Vann <dylan@dylanvann.com>
2018-07-26 13:45:41 -0400

    Add options argument to requests.

--- src/index.ts
+++ src/index.ts
@@ -21,29 +21,45 @@ export const isVideo = (localAbsolutePath: string) => {
 
 const uploadPromised = promisify(cloudinary.v2.uploader.upload)
 
-export const uploadFile = (id: string, localAbsolutePath: string): Promise<any> =>
+export const uploadFile = (
+    id: string,
+    localAbsolutePath: string,
+    options: any,
+): Promise<any> =>
     uploadPromised(localAbsolutePath, {
         public_id: id,
         resource_type: isVideo(localAbsolutePath) ? 'video' : 'image',
+        ...options,
     })
 
 const uploadExplicitPromised = promisify(cloudinary.v2.uploader.explicit)
 
-export const getMetadata = (id: string, localAbsolutePath: string): Promise<any> =>
+export const getMetadata = (
+    id: string,
+    localAbsolutePath: string,
+    options: any,
+): Promise<any> =>
     uploadExplicitPromised(id, {
         image_metadata: true,
         type: 'upload',
         resource_type: isVideo(localAbsolutePath) ? 'video' : 'image',
+        ...options,
     })
 
-export const imageExists = (id: string, config: CloudinaryConfig): Promise<boolean> => {
+export const imageExists = (
+    id: string,
+    config: CloudinaryConfig,
+): Promise<boolean> => {
     const urlImg = `http://res.cloudinary.com/${
         config.cloud_name
     }/image/upload/${id}`
     return urlExists(urlImg)
 }
 
-export const videoExists = (id: string, config: CloudinaryConfig): Promise<boolean> => {
+export const videoExists = (
+    id: string,
+    config: CloudinaryConfig,
+): Promise<boolean> => {
     const urlVideo = `http://res.cloudinary.com/${
         config.cloud_name
     }/video/upload/${id}`
@@ -65,13 +81,14 @@ export const uploadOrGetMetadata = async (
     id: string,
     localAbsolutePath: string,
     config: CloudinaryConfig,
+    options: any,
 ) => {
     const exists = await fileExists(id, localAbsolutePath, config)
     if (!exists) {
         // Have to upload the image or video
-        return uploadFile(id, localAbsolutePath)
+        return uploadFile(id, localAbsolutePath, options)
     } else {
         // Already uploaded, we just get the metadata
-        return await getMetadata(id, localAbsolutePath)
+        return await getMetadata(id, localAbsolutePath, options)
     }
 }
